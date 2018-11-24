45296567d70c0c8db6995b7971b6b2b282b6ea2b
Alexander Vo <randomcivvy0121@gmail.com>
2018-11-24 15:08:00 -0700

    Create sprite example

--- js/main.js
+++ js/main.js
@@ -2,20 +2,47 @@ var THREE = require('three');
 
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+//var camera = new THREE.OrthographicCamera(window.innerWidth / window.innerHeight, 0.1, 1000);
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+var spriteMap = new THREE.TextureLoader().load("image/test.jpg");
+var spriteMaterial = new THREE.SpriteMaterial( { map: spriteMap, color: 0xffffff } );
+var sprite = new THREE.Sprite( spriteMaterial );
 
+var sprite2 = new THREE.Sprite( spriteMaterial.clone() );
+
+sprite.position.x = 0;
+sprite.position.y = 0;
+sprite.position.z = 0;
+scene.add( sprite );
+
+sprite2.position.x = 0;
+sprite2.position.y = -0.5;
+sprite2.position.z = -1;
+
+scene.add( sprite2 );
+
+camera.position.x = 0;
+camera.position.y = 0;
 camera.position.z = 5;
 
+//camera.lookAt(sprite);
+
+var foo = false;
 function animate() {
 	requestAnimationFrame( animate );
+
+  if (sprite.material.rotation < 3.1) {
+    sprite.material.rotation += 0.1;
+  } else {
+    if (foo == false) {
+      sprite.material = sprite2.material.clone();
+    }
+  }
+
 	renderer.render( scene, camera );
 }
 animate();
