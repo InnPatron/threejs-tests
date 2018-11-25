c30cba19752c32640e406be6dc27dd8f618583a0
Alexander Vo <randomcivvy0121@gmail.com>
2018-11-24 18:23:24 -0700

    Add matter-js() example

--- js/main.js
+++ js/main.js
@@ -52,4 +52,37 @@ function threejs() {
   animate();
 }
 
-threejs();
+function matterjs() {
+  var MATTER = require('matter-js');
+  // module aliases
+  var Engine = MATTER.Engine,
+      Render = MATTER.Render,
+      World = MATTER.World,
+      Bodies = MATTER.Bodies;
+
+  // create an engine
+  var engine = Engine.create();
+
+  // create a renderer
+  var render = Render.create({
+      element: document.body,
+      engine: engine
+  });
+
+  // create two boxes and a ground
+  var boxA = Bodies.rectangle(400, 400, 80, 80);
+  var boxB = Bodies.rectangle(450, 450, 80, 80);
+  var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+
+  // add all of the bodies to the world
+  World.add(engine.world, [boxA, boxB, ground]);
+
+  // run the engine
+  Engine.run(engine);
+
+  // run the renderer
+  Render.run(render);
+}
+
+// threejs();
+matterjs();
