9af48f141ee29fa586f96f8776060ac14b056a10
Alexander Vo <randomcivvy0121@gmail.com>
2018-11-25 16:20:41 -0700

    Implement primitive matterjs rendering with threejs

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@ function threejs() {
   var THREE = require('three');
 
   var scene = new THREE.Scene();
-  var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-  //var camera = new THREE.OrthographicCamera(window.innerWidth / window.innerHeight, 0.1, 1000);
+  //var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+  var camera = new THREE.OrthographicCamera(window.innerWidth / window.innerHeight, 0.1, 1000);
 
   var renderer = new THREE.WebGLRenderer();
   renderer.setSize( window.innerWidth, window.innerHeight );
@@ -31,7 +31,7 @@ function threejs() {
 
   camera.position.x = 0;
   camera.position.y = 0;
-  camera.position.z = 5;
+  camera.position.z = -5;
 
   //camera.lookAt(sprite);
 
@@ -84,5 +84,88 @@ function matterjs() {
   Render.run(render);
 }
 
+function matterthree() {
+  var THREE = require('three');
+  var MATTER = require('matter-js');
+
+  // Set up threejs
+  var scene = new THREE.Scene();
+  var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+  var renderer = new THREE.WebGLRenderer();
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  document.body.appendChild( renderer.domElement );
+
+  // Set up threejs object
+  var spriteMap = new THREE.TextureLoader().load("image/test.jpg");
+  var spriteMaterial = new THREE.SpriteMaterial( { map: spriteMap, color: 0xffffff } );
+  spriteMaterial.sizeAttenuation = false;
+
+  var sprite = new THREE.Sprite( spriteMaterial );
+  sprite.scale.set(0.1, 0.1, 1);
+
+  sprite.position.x = 2;
+  sprite.position.y = 8;
+  sprite.position.z = 0;
+  scene.add( sprite );
+
+  camera.position.x = 0;
+  camera.position.y = 0;
+  camera.position.z = 40;
+
+  camera.lookAt(new THREE.Vector3(0, 0, 1));
+
+  
+  var groundGeometry = new THREE.BoxGeometry( 20, 5, 0.1 );
+  var groundMaterial = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+  var ground = new THREE.Mesh( groundGeometry, groundMaterial );
+  ground.position.y = -4;
+  scene.add( ground );
+
+  
+  // Matter module aliases
+  var Engine = MATTER.Engine,
+      Render = MATTER.Render,
+      World = MATTER.World,
+      Bodies = MATTER.Bodies;
+
+  // create an engine
+  var engine = Engine.create();
+
+  // Create collision box for sprite
+  var spriteCollider = Bodies.rectangle(2, -8, 10, 10);
+  spriteCollider.restitution = 0;
+  var groundCollider = Bodies.rectangle(0, 4, 20, 2, { isStatic: true });
+  groundCollider.restitution = 0;
+
+  // add all of the bodies to the world
+  World.add(engine.world, [spriteCollider, groundCollider]);
+
+  // run the engine
+  Engine.run(engine);
+
+  /*
+  var render = Render.create({
+      element: document.body,
+      engine: engine
+  });
+  */
+
+  // Render.run(render);
+
+  function animate() {
+    requestAnimationFrame( animate );
+
+    // sprite.position.x = spriteCollider.position.x;
+    sprite.position.y = -spriteCollider.position.y;
+
+    // console.log(spriteCollider.position.y);
+    renderer.render( scene, camera );
+  }
+  
+  animate();
+}
+
 // threejs();
-matterjs();
+// matterjs();
+matterthree();
